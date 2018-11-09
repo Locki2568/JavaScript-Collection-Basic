dc8391bc05ca7f172aa672bccb1d62f752b3318d
Locki Lo <locki.lo@cargosmart.com>
2018-11-09 17:36:08 +0800

    section 1 : practice 2,3,4

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,24 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  var result = [];
+  for(var i=0; i<collectionA.length; i++){
+    for(var j=0; j<collectionB.length; j++){
+      var subCollection = collectionB[j];
+      var searchResult = findElementInCollection(collectionA[i],subCollection);
+      if (searchResult != null){
+        result.push(searchResult)
+      }
+    }
+  }
+  return result;
+}
+
+function findElementInCollection(target, subCollection){
+  for(var i=0; i<subCollection.length; i++){
+      if(subCollection[i] === target){
+        return target;
+      }
+  }
+  return null;
 }
--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,5 +1,23 @@
 'use strict';
 
-function collectSameElements(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+function collectSameElements(collectionA, collectionB) {
+  var result = [];
+  var subCollection = collectionB.value;
+  for(var i=0; i<collectionA.length; i++){
+      var targetValue = collectionA[i].key
+      var searchResult = findElementInCollection(targetValue,subCollection);
+      if (searchResult != null){
+        result.push(searchResult)
+      }
+  }
+  return result;
+}
+
+function findElementInCollection(target, subCollection){
+  for(var i=0; i<subCollection.length; i++){
+      if(subCollection[i] === target){
+        return target;
+      }
+  }
+  return null;
 }
