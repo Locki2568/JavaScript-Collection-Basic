8dcd7fbd7e8f536873fcfc88da32d95d786ed8e7
Locki Lo <locki.lo@cargosmart.com>
2018-11-12 18:12:21 +0800

    JavaScript section 3 - p1

--- practice/section-3/practice-1.js
+++ practice/section-3/practice-1.js
@@ -1,5 +1,22 @@
 'use strict';
 
+var output = [];
+
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  for (var i=0; i<collectionA.length; i++){
+    var elementA = collectionA[i].key
+    if (sreachForObjectValue(elementA, objectB)){
+      output.push({key:elementA, count:1})
+    }else{
+      output.push(collectionA[i])
+    }
+  }
+  return output;
 }
+function sreachForObjectValue(elementA, objectB){
+    if (objectB.value.find(v => v === elementA)){
+      return true
+    }else{
+      return false
+    }
+  }
