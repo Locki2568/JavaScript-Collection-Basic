2755007920cfd10a751112fa36f5e638c16ef250
Locki Lo <locki.lo@cargosmart.com>
2018-11-13 17:23:28 +0800

    JavaScript section 3 practice 2,3,4

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,5 +1,40 @@
 'use strict';
 
+var output = [];
+
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  for (var i=0; i<collectionA.length; i++){
+    var elementA = collectionA[i].key
+    var countA = parseInt(collectionA[i].count)
+    if (sreachForObjectValue(elementA, objectB)){
+      countA = minus1ForEvery3(countA)
+      output.push({key:elementA, count:countA})
+    }else{
+      output.push(collectionA[i])
+    }
+  }
+  console.log(output)
+  return output;
 }
+
+function minus1ForEvery3(countA){
+  var count = countA
+  var numOfminus1 = 0
+  if(countA < 3){
+    return countA
+  }else{
+    while(count >= 3){
+      numOfminus1 += 1 
+      count -=3
+    }
+    return countA + (numOfminus1 * -1)
+  }
+}
+
+function sreachForObjectValue(elementA, objectB){
+    if (objectB.value.find(v => v === elementA)){
+      return true
+    }else{
+      return false
+    }
+  }
--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,5 +1,69 @@
 'use strict';
 
+
+var summary = [];
+var output = [];
+var distinctElement = [];
+
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  countElement(collectionA)
+  console.log(summary)
+  for (var i=0; i<summary.length; i++){
+    var elementA = summary[i].key
+    var countA = parseInt(summary[i].count)
+    if (sreachForObjectValue(elementA, objectB)){
+      countA = minus1ForEvery3(countA)
+      output.push({key:elementA, count:countA})
+    }else{
+      output.push(summary[i])
+    }
+  }
+  return output
+}
+
+function minus1ForEvery3(countA){
+  var count = countA
+  var numOfminus1 = 0
+  if(countA < 3){
+    return countA
+  }else{
+    while(count >= 3){
+      numOfminus1 += 1 
+      count -=3
+    }
+    return countA + (numOfminus1 * -1)
+  }
+}
+
+function sreachForObjectValue(elementA, objectB){
+    if (objectB.value.find(v => v === elementA)){
+      return true
+    }else{
+      return false
+    }
+  }
+
+function countElement(collectionA){
+  for(var i=0; i<collectionA.length; i++){
+    var element = collectionA[i]
+    if (isElementDistinct(element)){
+      distinctElement.push(element)
+      summary.push({key: element, count: 1})
+    }else{
+      updateCount(element)
+    }
+  }
+}
+
+function isElementDistinct(element){
+  if(distinctElement.indexOf(element) != -1){
+    return false
+  }else{
+    return true
+  }
+}
+
+function updateCount(element){
+  var originCount = summary.find(x => x.key === element).count
+  summary.find(x => x.key === element).count = (originCount + 1)
 }
